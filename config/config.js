5a8f82ae683e003a1d0c5f7720d55012864abb02
lorenir <irennn2002@gmail.com>
2019-09-08 20:16:50 +0200

    Autenticacion con google

--- config/config.js
+++ config/config.js
@@ -1,1 +1,6 @@
 module.exports.SEED = '@este-es-mi-seed';
+
+//Google
+module.exports.CLIENT_ID = '211801276626-jlcqrg85h4j57u2er2teka79v0nnumfq.apps.googleusercontent.com';
+
+module.exports.CLIENT_ID_SECRET = 't-iGLKKRMCEHkfhTpLJUn8ib';
--- routes/login.js
+++ routes/login.js
@@ -10,6 +10,124 @@ var app = express();
 //Rutas
 var Usuario = require('../models/usuario');
 
+//Google:
+var CLIENT_ID = require('../config/config').CLIENT_ID;
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID);
+
+//========================================
+// Autenticación google
+//========================================
+
+async function verify(token) {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+
+    const payload = ticket.getPayload();
+    // const userid = payload['sub'];
+    // If request specified a G Suite domain:
+    //const domain = payload['hd'];
+
+    return {
+        nombre: payload.name,
+        email: payload.email,
+        img: payload.picture,
+        // payload: payload,
+        google: true
+    };
+}
+
+
+app.post('/google', async(req, res) => {
+
+    var token = req.body.token;
+    var googleUser = await verify(token)
+        .catch(e => {
+            res.status(503).json({
+                ok: false,
+                mensaje: 'Token no válido'
+            });
+        });
+
+
+    Usuario.findOne({ email: googleUser.email }, (err, usuarioBD) => {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al bucar usuarios',
+                errors: err
+            });
+        }
+
+        if (usuarioBD) {
+
+            if (usuarioBD.google === flase) {
+
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'Debe usar su autenticación normal'
+                });
+            } else {
+                // CREAR TOKEN
+                var token = jwt.sign({ usuario: usuarioBD }, SEED, { expiresIn: 14400 }); // 4h
+
+                res.status(200).json({
+                    ok: true,
+                    usuario: usuarioBD,
+                    token: token,
+                    id: usuarioBD.id
+                });
+            }
+
+        } else {
+            // el suario no existe, hay que crearlo
+            var usuario = new Usuario();
+
+            usuario.nombre = googleUser.nombre;
+            usuario.email = googleUser.email;
+            usuario.img = googleUser.img;
+            usuario.google = true;
+            usuario.password = '***';
+
+            usuario.save((err, usuarioBD) => {
+                if (err) {
+                    return res.status(500).json({
+                        ok: false,
+                        mensaje: 'Error guardando usuario de Google en DB',
+                        errors: err
+                    });
+                }
+
+                // CREAR TOKEN
+                var token = jwt.sign({ usuario: usuarioBD }, SEED, { expiresIn: 14400 }); // 4h
+
+                res.status(200).json({
+                    ok: true,
+                    usuario: usuarioBD,
+                    token: token,
+                    id: usuarioBD.id
+                });
+            });
+        }
+    });
+
+    // res.status(200).json({
+    //     ok: true,
+    //     mensaje: 'OK!!!',
+    //     googleUser: googleUser
+    // });
+});
+
+
+//========================================
+// Autenticación normal
+//========================================
+
 app.post('/', (req, res) => {
 
     var body = req.body;
@@ -54,11 +172,6 @@ app.post('/', (req, res) => {
 
     });
 
-
-
 });
-
-
-
 
 module.exports = app;
